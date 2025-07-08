650f5a578c1593f8565af0d9a200787a6c95b57f
Muhammad Qasim <154578365+mqasimkh@users.noreply.github.com>
2025-07-08 11:43:01 +0500

    ...

--- code/router_module_uvc/router_module_env.sv
+++ code/router_module_uvc/router_module_env.sv
@@ -4,8 +4,17 @@ class router_module_env extends uvm_env;
     router_reference reference_model;
     router_scoreboard scoreboard;
 
+    uvm_analysis_export #(yapp_packet) ref_model_yapp;
+    uvm_analysis_export #(hbus_transacton) ref_model_hbus;
+
+    uvm_analysis_export #(channel_packet) scr_chan0;
+    uvm_analysis_export #(channel_packet) scr_chan1;
+    uvm_analysis_export #(channel_packet) scr_chan2;
+
+
     function new (string name = "router_module_env", uvm_component parent);
         super.new(name, parent);
+        ref_model_yapp = 
     endfunction: new
 
     function void build_phase(uvm_phase phase);
